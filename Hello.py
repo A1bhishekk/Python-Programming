31cdc979dff67994ccd6e060fb3a00b74fed2eb6
Abhi <sahniak56@gmail.com>
2022-09-06 07:14:51 +0530

    Starting Python From 6 September 20222

---
+++ Hello.py
@@ -0,0 +1,9 @@
+print("Hello world");
+
+# function 
+def greet(name):
+    {
+        print(f"Good Morning ! {name}") #string literal
+    }
+
+greet("Abhishek");
---
+++ String/String.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+a=int(input("Enter first No. :"))
+b=int(input("Enter second No. :"))
+avg=(a+b)/2
+print("Average of a & b is ", avg)
